cb9a30ec8ebe1e2258afe000a19bf97885e8a9eb
Alin <alinm4393@gmail.com>
2024-06-04 17:47:48 +0300

    test commit - AppointmentRepository class

--- src/main/java/org/example/entity/Consult.java
+++ src/main/java/org/example/entity/Consult.java
@@ -1,2 +1,40 @@
-package org.example.entity;public class Consult {
+package org.example.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Consult {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private int consultId;
+
+    @OneToOne
+    @JoinColumn
+    private Appointment appointment;
+
+    private String notes;
+
+    public int getConsultId() {
+        return consultId;
+    }
+
+    public void setConsultId(int consultId) {
+        this.consultId = consultId;
+    }
+
+    public Appointment getAppointment() {
+        return appointment;
+    }
+
+    public void setAppointment(Appointment appointment) {
+        this.appointment = appointment;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
