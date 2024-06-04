9e827950f6234035f6a4419367db1acb9785143f
Alin <alinm4393@gmail.com>
2024-06-04 17:48:35 +0300

    test commit - AppointmentRepository class

--- src/main/java/org/example/entity/Appointment.java
+++ src/main/java/org/example/entity/Appointment.java
@@ -1,2 +1,73 @@
-package org.example.entity;public class Appointment {
+package org.example.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Appointment {
+
+    @Id
+    @GeneratedValue ( strategy = GenerationType.IDENTITY)
+    private int appointmentId ;
+
+    private LocalDateTime dateTime ;
+
+
+    @ManyToOne
+    @JoinColumn
+    private Vet vet;
+
+    @ManyToOne
+    @JoinColumn
+    private Pet pet;
+
+    private boolean isCanceled;
+
+    @OneToOne (mappedBy = "appointment")
+    private Consult consult;
+
+    public Consult getConsult() { return consult; }
+
+    public void setConsult(Consult consult) { this.consult = consult;}
+
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public boolean isCanceled() {
+        return isCanceled;
+    }
+
+    public void setCanceled(boolean canceled) {
+        isCanceled = canceled;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public Vet getVet() {
+        return vet;
+    }
+
+    public void setVet(Vet vet) {
+        this.vet = vet;
+    }
 }
