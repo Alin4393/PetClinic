6e0d09279bb607876709ddefdf8c8eb3eb60d159
Alin <alinm4393@gmail.com>
2024-06-04 17:48:12 +0300

    test commit - AppointmentRepository class

--- src/main/java/org/example/entity/Pet.java
+++ src/main/java/org/example/entity/Pet.java
@@ -1,2 +1,76 @@
-package org.example.entity;public class Pet {
+package org.example.entity;
+
+import jakarta.persistence.*;
+import org.example.enums.PetType;
+
+import java.util.List;
+
+@Entity
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int petId;
+    private String name;
+
+    @Enumerated(value = EnumType.STRING)
+    private PetType petType;
+
+    @Column(unique = true)
+    private String collarId;
+
+    @OneToMany (mappedBy = "pet")
+    private List<Appointment> appointment;
+
+    public List<Appointment> getAppointment() {
+        return appointment;
+    }
+
+    public void setAppointment(List<Appointment> appointment) {
+        this.appointment = appointment;
+    }
+
+    @ManyToOne
+    @JoinColumn
+    private PetOwner petOwner;
+
+    public String getCollarId() {
+        return collarId;
+    }
+
+    public void setCollarId(String collarId) {
+        this.collarId = collarId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPetId() {
+        return petId;
+    }
+
+    public void setPetId(int petId) {
+        this.petId = petId;
+    }
+
+    public PetOwner getPetOwner() {
+        return petOwner;
+    }
+
+    public void setPetOwner(PetOwner petOwner) {
+        this.petOwner = petOwner;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
 }
